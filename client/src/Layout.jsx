5a4e604b19d3344be76fdd957feb56f3de847445
MercyNJ <mercinjuguna@gmail.com>
2024-06-06 10:57:24 +0000

    Added an About Me section & page

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -3,13 +3,16 @@ import CategorySubheader from "./CategorySubheader";
 import Footer from "./Footer";
 import BannerImage from "./BannerImage";
 import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function Layout() {
+	const location = useLocation();
+
   return (
     <>
       <Header />
       <CategorySubheader /> 
-      <BannerImage />
+      {location.pathname !== "/about" && <BannerImage />}
       <main>
         <Outlet />
       </main>

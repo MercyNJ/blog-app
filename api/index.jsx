9498ee0ea9442a9b30a9f73d4377ecd681642774
MercyNJ <mercinjuguna@gmail.com>
2024-06-12 16:55:28 +0000

    Adjusting code to prepare for deployment

--- api/index.jsx
+++ api/index.jsx
@@ -304,4 +304,9 @@ app.delete('/comment/:id', async (req, res) => {
   }
 });
 
-app.listen(3000);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+

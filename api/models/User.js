ae1c27be7be33862450949022c29f07e4de571bb
MercyNJ <mercinjuguna@gmail.com>
2026-06-06 00:51:10 +0300

    Set up Sequelize migrations and extend user schema

--- api/models/User.js
+++ api/models/User.js
@@ -5,14 +5,30 @@ const UserModel = sequelize.define('User', {
   username: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
     validate: {
-      len: [4]
-    },
-    unique: true
+      len: [4, 255]
+    }
   },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+
   password: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+
+  role: {
+    type: DataTypes.ENUM('admin', 'user'),
+    allowNull: false,
+    defaultValue: 'user'
   }
 });
 
